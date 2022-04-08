6178c62485533186d990171df075d570cd585b5f
DerekYeung <466836531@qq.com>
2022-04-08 14:10:50 +0800

    u

--- client.js
+++ client.js
@@ -1,23 +1,41 @@
 'use strict';
 
 const { io } = require("socket.io-client");
-// const socket = io("ws://43.129.225.40:7300");
-const socket = io("ws://127.0.0.1:7001");
+const socket = io("ws://43.129.225.40:7300");
+// const socket = io("ws://127.0.0.1:7001");
 
+const RECORDS = require('./uni.json').RECORDS;
+console.log(RECORDS.length);
 
 socket.on('connect', () => {
   console.log('on ok');
-  socket.emit('quote', {
-    from: {
-      contract: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-      decimals: 6
-    },
-    to: {
-      contract: '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2',
-      decimals: 18
-    },
-    amount: 15000
-  }, res => {
-    console.log(res);
-  });
+  RECORDS.forEach(node => {
+    socket.emit('quote', {
+      from: {
+        contract: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+        decimals: 6
+      },
+      to: {
+        contract: node.contract,
+        decimals: node.decimals
+      },
+      amount: 15000
+    }, res => {
+      console.log(res);
+    });
+    socket.emit('quote', {
+      from: {
+        contract: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+        decimals: 6
+      },
+      to: {
+        contract: node.contract,
+        decimals: node.decimals
+      },
+      amount: 15000
+    }, res => {
+      console.log(res);
+    });
+
+  })
 })
