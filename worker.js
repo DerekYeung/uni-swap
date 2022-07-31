ec2f3dde15e19decd5b6012d931c7ef74c416f69
DerekYeung <466836531@qq.com>
2022-08-01 01:08:22 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -68,6 +68,9 @@ async function quote({
         error: 'Missing route'
       };
     }
+    console.log(`Quote Exact In: ${route.quote.toFixed(2)}`);
+    console.log(`Gas Adjusted Quote In: ${route.quoteGasAdjusted.toFixed(2)}`);
+    console.log(`Gas Used USD: ${route.estimatedGasUsedUSD.toFixed(6)}`);
     // console.log(`Quote Exact In: ${route.quote.toFixed(2)}`);
     // console.log(`Gas Adjusted Quote In: ${route.quoteGasAdjusted.toFixed(2)}`);
     // console.log(`Gas Used USD: ${route.estimatedGasUsedUSD.toFixed(6)}`);
